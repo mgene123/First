939398b9a60b1fbe4959aad892e35fd219b84e82
mgene123 <36472508+mgene123@users.noreply.github.com>
2018-02-14 12:39:41 +0200

    this is first commit and first project

---
+++ WebService1/AddOperation.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService1
+{
+    /// <summary>
+    /// Summary description for AddOperation
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AddOperation : System.Web.Services.WebService
+    {
+        public secured SoapHeader;
+
+        [WebMethod]
+        [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+        public float Add (float FirstNumber,float SecondNumber)
+        {
+            
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
